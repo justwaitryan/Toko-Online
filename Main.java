164de7737d907c1b8ba010d32da567f05ec2e263
Dhiriyan Firdaus <133236112+justwaitryan@users.noreply.github.com>
2024-02-28 11:49:39 +0700

    first commit

--- Main.java
+++ Main.java
@@ -11,8 +11,21 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        Transaksi transaksi = new Transaksi();
-        System.out.println(transaksi);
+        Barang barang = new Barang ();
+        Transaksi transaksi = new Transaksi ();
+        Member member = new Member();
+        Laporan laporan = new Laporan ();
+        
+        laporan.laporan(barang);
+        laporan.laporan(member);
+        laporan.laporan(transaksi, barang);
+        
+        transaksi.prosesTransaksi(member, transaksi, barang);
+        
+        laporan.laporan(transaksi, barang);
+        laporan.laporan(barang);
+        laporan.laporan(member);
+       
     }
     
 }
